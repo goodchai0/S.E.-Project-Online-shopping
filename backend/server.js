8dd3c67443dc24a206d4518b8a5a5df8a7eafafd
Amit <www.amitkewot@gmail.com>
2023-04-19 02:08:30 +0530

    wip

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 // if (process.env.NODE_ENV === "production") {
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "./frontend/build")));
 
 app.get("/*", (req, res) =>
   res.sendFile(path.join(__dirname, "build", "index.html"))
